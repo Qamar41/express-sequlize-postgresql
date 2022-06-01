b6a5f24e428ceb8b75419d4aa422faa00742c647
mubi-janjua <mubeenaftabjanjua@gmail.com>
2022-06-01 13:15:02 +0500

    "product api completed"

--- controllers/product.js
+++ controllers/product.js
@@ -6,48 +6,72 @@ const Asset=db.Asset
 
 exports.buyProduct = async (req, res) => {
     const data = req.body;
-    const catalogId=data.id;
-    const UserWalletAddress=data.address;
-    const response={}                  
+    const catalogId = data.id;
+    const UserWalletAddress = data.address;
+    const response = {}
     try {
         // get catalog
-            const catalog =await Catalog.findByPk(catalogId)
-            if (!catalog) {
-                response['message']='catalog not found'
-                return res.send(response).status(404);
-            }
-            console.log(catalog)
+        const catalog = await Catalog.findByPk(catalogId)
+        if (!catalog) {
+            response['message'] = 'catalog not found'
+            return res.send(response).status(404);
+        }
+        console.log(catalog)
         // get user
-            const user =await User.findByPk(UserWalletAddress)
-            if (!user) {
-                response['message']='user not found'
-                return res.send(response).status(404);
-            }
-            if (catalog!==null && user !==null){
-                const case_1=user.cash1<=catalog.cost1 && user.cash2<=catalog.cash2 && user.cash3 <=catalog.cash3
-                if (case_1){
-                    const reqCheck= catalog.req1 !== null && catalog.req2 !==null && catalog.req3 !==null
-                    if (reqCheck){
-                        const checkAsset=Asset.findOne({ where: { address: user.address } })
+        const user = await User.findByPk(UserWalletAddress)
+        if (!user) {
+            response['message'] = 'user not found'
+            return res.send(response).status(404);
+        }
+        if (catalog !== null && user !== null) {
+            const case_1 = user.cash1 <= catalog.cost1 && user.cash2 <= catalog.cost2 && user.cash3 <= catalog.cost3;
+            if (case_1) {
+                response['success'] = false
+                let reqValue = null;
+                if (data.requirementType === 'req1') {
+                    if (catalog.req1 === null) {
+                        response['errorMessage'] = 'catalog request 1 is null'
+                        return res.send(response).status(404);
                     }
-                    console.log('Case_1 ',case_1)
-                    response['success']=false
-                    response['errorMessage']='error occured'
+                    reqValue = catalog.req1;
+                } else if (data.requirementType === 'req2') {
+                    if (catalog.req2 === null) {
+                        response['errorMessage'] = 'catalog request 2 is null'
+                        return res.send(response).status(404);
+                    }
+                    reqValue = catalog.req2;
+                } else if (data.requirementType === 'req3') {
+                    if (catalog.req3 === null) {
+                        response['errorMessage'] = 'catalog request 3 is null'
+                        return res.send(response).status(404);
+                    }
+                    reqValue = catalog.req3;
+                } else {
+                    response['errorMessage'] = 'please enter catalog request type'
+                    return res.send(response).status(400);
                 }
-                if( user.cash1>catalog.cost1 && user.cash2>catalog.cost2 && user.cash3 >catalog.cost3){
-                    user.cash1=user.cash1-catalog.cost1
-                    user.cash2=user.cash2-catalog.cost2
-                    user.cash3=user.cash3-catalog.cost3
-                    await user.save()
-                    const product=await Product.create({address:user.address,id:67,userAddress:user.address})
-                    response['success']=true
-                    response['data']={resources:{cash1:user.cash1 , cash2:user.cash2 , cash3:user.cash3}}
+                // const reqCheck= catalog.req1 !== null || catalog.req2 !==null || catalog.req3 !==null
+                if (reqValue !== null) {
+                    const checkAsset = await Asset.findOne({where: {userAddress: user.address, type: data.type}});
+                    if (!checkAsset) {
+                        response['errorMessage'] = 'asset not found'
+                        return res.send(response).status(404);
+                    }
+                    if (checkAsset.level <= reqValue)
+                        response['errorMessage'] = 'level is less than requirement'
                 }
-                else{
-                    response['message']="you are out of ballance"
-                    return res.send(response).status(404);
-                }
+                return res.send(response).status(400);
             }
+        if (user.cash1 > catalog.cost1 && user.cash2 > catalog.cost2 && user.cash3 > catalog.cost3) {
+            user.cash1 = user.cash1 - catalog.cost1
+            user.cash2 = user.cash2 - catalog.cost2
+            user.cash3 = user.cash3 - catalog.cost3
+            await user.save()
+            const product = await Product.create({address: user.address, id: 67, userAddress: user.address})
+            response['success'] = true
+            response['data'] = {resources: {cash1: user.cash1, cash2: user.cash2, cash3: user.cash3}}
+        }
+    }
         return res.send(response).status(200);
     } catch (err) {
         console.log(err)
